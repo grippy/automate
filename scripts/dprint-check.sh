f265aaeb41626869e9a2413ecd97e8a572dc2837
Greg Melton <gmelton@gmail.com>
2023-03-20 12:39:44 -0700

    adding dprint checks to deno.yaml

---
+++ scripts/dprint-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# TODO: check back later if we make this a deno task
+#       (requires glob support)
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+AUTOMATE_CORE=${AUTOMATE_CORE:-$SCRIPT_DIR/..}
+ARGS="./core/*.ts ./core/**/*.ts ./core/**/**/*.ts ./cli/*.ts ./cli/**/*.ts ./cli/**/**/*.ts --excludes ./cli/template/"
+
+echo 'dprint checking these files...'
+cd $AUTOMATE_CORE
+dprint output-file-paths $ARGS | sort
+echo '---'
+dprint check $ARGS
+echo 'dprint done checking files'
